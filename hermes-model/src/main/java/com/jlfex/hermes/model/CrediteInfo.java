9e8ef3d47a194fbec5baab475cd2fdc20aa8ba3b
sunkailin <sunkailin@jinlufund.com>
2015-01-23 11:22:01 +0800

    #310

--- hermes-model/src/main/java/com/jlfex/hermes/model/CrediteInfo.java
+++ hermes-model/src/main/java/com/jlfex/hermes/model/CrediteInfo.java
@@ -2,13 +2,16 @@ package com.jlfex.hermes.model;
 
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+
 import com.jlfex.hermes.common.App;
 import com.jlfex.hermes.common.AppUser;
 import com.jlfex.hermes.common.Logger;
@@ -31,10 +34,10 @@ public class CrediteInfo extends Model {
 	@ManyToOne
 	@JoinColumn(name = "creditor")
 	private Creditor creditor;
-	//债权编号
+	// 债权编号
 	@Column(name = "credit_code")
 	private String crediteCode;
-	//债权类型
+	// 债权类型
 	@Column(name = "credite_type")
 	private String crediteType;
 	// 借款人(债权的原始借款人)
@@ -60,16 +63,16 @@ public class CrediteInfo extends Model {
 	private BigDecimal amount;
 	// 年利率
 	@Column(name = "rate")
-	private BigDecimal rate ;
-	//原始 ： 借款期限
+	private BigDecimal rate;
+	// 原始 ： 借款期限
 	@Column(name = "period")
-	private Integer  period ;
+	private Integer period;
 	// 招标期限
 	private Integer deadLine;
-	//剩余期数
+	// 剩余期数
 	@Column(name = "term_num")
-	private Integer  termNum;
-	//借款用途
+	private Integer termNum;
+	// 借款用途
 	@Column(name = "purpose")
 	private String purpose;
 	// 还款方式
@@ -77,21 +80,21 @@ public class CrediteInfo extends Model {
 	private String payType;
 	// 债权到期日
 	@Column(name = "dead_time")
-	private Date deadTime ;
-	//放款日 (债权对应的原始借款人还款)
+	private Date deadTime;
+	// 放款日 (债权对应的原始借款人还款)
 	@Column(name = "business_time")
-	private Date businessTime ;
+	private Date businessTime;
 	@Column(name = "status")
-	private String status ;
-	//资金用途
+	private String status;
+	// 资金用途
 	@Column(name = "amount_aim")
-	private String  amountAim;
-	//产品介绍
+	private String amountAim;
+	// 产品介绍
 	@Column(name = "product_desc")
 	private String produnctDesc;
-	//担保方式
+	// 担保方式
 	@Column(name = "assure_type")
-	private String  assureType;
+	private String assureType;
 	// 招标结束日期
 	@Column(name = "bid_end_time")
 	private Date bidEndTime;
@@ -101,30 +104,26 @@ public class CrediteInfo extends Model {
 	// 债权 借款管理费
 	@Column(name = "manage_fee")
 	private BigDecimal manageFee;
-	
+
 	public Integer getTermNum() {
 		return termNum;
 	}
+
 	public void setTermNum(Integer termNum) {
 		this.termNum = termNum;
 	}
 
 	@Transient
-	private String  bidEndTimeStr;
+	private String bidEndTimeStr;
 	@Transient
-	private String  creditorName;
+	private String creditorName;
 	@Transient
-	private String  ratePercent;
+	private String ratePercent;
 	@Transient
-	private String  beginDate; //开始时间  用于检索导入时间段
+	private String beginDate; // 开始时间 用于检索导入时间段
 	@Transient
-	private String  endDate;  //结束时间   用于检索导入时间段
-	
-	
-	
+	private String endDate; // 结束时间 用于检索导入时间段
 
-    
-	
 	public Creditor getCreditor() {
 		return creditor;
 	}
@@ -212,6 +211,7 @@ public class CrediteInfo extends Model {
 	public void setRate(BigDecimal rate) {
 		this.rate = rate;
 	}
+
 	public Integer getPeriod() {
 		return period;
 	}
@@ -299,7 +299,7 @@ public class CrediteInfo extends Model {
 	public void setRemark(String remark) {
 		this.remark = remark;
 	}
-    
+
 	public String getBidEndTimeStr() {
 		return bidEndTimeStr;
 	}
@@ -307,7 +307,7 @@ public class CrediteInfo extends Model {
 	public void setBidEndTimeStr(String bidEndTimeStr) {
 		this.bidEndTimeStr = bidEndTimeStr;
 	}
-    
+
 	public BigDecimal getManageFee() {
 		return manageFee;
 	}
@@ -319,7 +319,7 @@ public class CrediteInfo extends Model {
 	public String getStatusName() {
 		return Dicts.name(status, status, Status.class);
 	}
-	
+
 	public String getCreditorName() {
 		return creditorName;
 	}
@@ -327,42 +327,46 @@ public class CrediteInfo extends Model {
 	public void setCreditorName(String creditorName) {
 		this.creditorName = creditorName;
 	}
-	
 
 	public Integer getDeadLine() {
 		return deadLine;
 	}
+
 	public void setDeadLine(Integer deadLine) {
 		this.deadLine = deadLine;
 	}
+
 	public String getRatePercent() {
-		if(rate !=null){
+		if (rate != null) {
 			rate = rate.multiply(new BigDecimal(100));
 		}
 		return rate.toString();
 	}
+
 	public String getDeadTimeFormate() {
 		String dateStr = "";
-		if(deadTime !=null ){
-			dateStr = new SimpleDateFormat("yyyy-MM-dd").format(deadTime); 
+		if (deadTime != null) {
+			dateStr = new SimpleDateFormat("yyyy-MM-dd").format(deadTime);
 		}
 		return dateStr;
 	}
-	
-	
 
 	public String getBeginDate() {
 		return beginDate;
 	}
+
 	public void setBeginDate(String beginDate) {
 		this.beginDate = beginDate;
 	}
+
 	public String getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(String endDate) {
 		this.endDate = endDate;
 	}
+
 	/**
 	 * 读取当前用户名称
 	 * 
@@ -379,6 +383,25 @@ public class CrediteInfo extends Model {
 		}
 	}
 
+	public Boolean getOutOfDate() {
+
+		Calendar c1 = Calendar.getInstance();
+		c1.setTime(deadTime);
+		c1.set(Calendar.MINUTE, 0);
+		c1.set(Calendar.SECOND, 0);
+		c1.set(Calendar.HOUR_OF_DAY, 0);
+		c1.set(Calendar.MILLISECOND, 0);
+
+		Calendar c2 = Calendar.getInstance();
+		c1.set(Calendar.MINUTE, 0);
+		c1.set(Calendar.SECOND, 0);
+		c1.set(Calendar.HOUR_OF_DAY, 0);
+		c1.set(Calendar.MILLISECOND, 0);
+
+		return c1.getTimeInMillis() < c2.getTimeInMillis();
+
+	}
+
 	public static final class Status {
 		@Element("待发售")
 		public static final String WAIT_ASSIGN = "00";
@@ -387,7 +410,7 @@ public class CrediteInfo extends Model {
 		@Element("还款中")
 		public static final String REPAYING = "02";
 		@Element("已还清")
-		public static final String REPAY_FIINISH= "03";
+		public static final String REPAY_FIINISH = "03";
 		@Element("验证失败")
 		public static final String IMP_FAIL = "04";
 		@Element("转让失败")
