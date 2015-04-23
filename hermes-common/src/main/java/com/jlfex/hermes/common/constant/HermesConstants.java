823ac9a1dc8da8ec3621bb23d889fa0d67d87a3e
iswu2013 <1037216275@qq.com>
2015-04-23 16:08:14 +0800

    账户类型为11更明确！

--- hermes-common/src/main/java/com/jlfex/hermes/common/constant/HermesConstants.java
+++ hermes-common/src/main/java/com/jlfex/hermes/common/constant/HermesConstants.java
@@ -160,7 +160,7 @@ public final class HermesConstants {
 	/**
 	 * 中金账户类型,个人
 	 */
-	public static final int ZJ_ACCOUNT_TYPE = 11;
+	public static final int ZJ_ACCOUNT_TYPE_11 = 11;
 	public static final int ZJ_ACCOUNT_TYPE_12 = 12;
 	public static final int ZJ_ACCOUNT_TYPE_20 = 20;
 
